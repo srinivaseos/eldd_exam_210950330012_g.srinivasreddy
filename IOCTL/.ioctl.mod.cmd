ad44fbf35f5522e07200711014ad78eac625b2ad
srinivaseos <srinivasreddy9434@gmail.com>
2022-01-10 12:54:43 +0530

    question1 ioctl

---
+++ IOCTL/.ioctl.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/srinu/Documents/eldd_exam_210950330012_g.srinivasreddy/IOCTL/ioctl.mod := { echo  /home/srinu/Documents/eldd_exam_210950330012_g.srinivasreddy/IOCTL/ioctl.o;  echo; } > /home/srinu/Documents/eldd_exam_210950330012_g.srinivasreddy/IOCTL/ioctl.mod
---
+++ IOCTL/ioctl.mod
@@ -0,0 +1,2 @@
+/home/srinu/Documents/eldd_exam_210950330012_g.srinivasreddy/IOCTL/ioctl.o
+
